28e67110790741bf3245400ab8a1c40895475c4f
YOYO <hello@YOYO-MacBook.local>
2025-10-12 22:56:14 +0700

    Update ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,79 +6,51 @@
 /*   By: ssaensuk <ssaensuk@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 19:11:47 by ssaensuk          #+#    #+#             */
-/*   Updated: 2025/10/11 21:05:00 by ssaensuk         ###   ########.fr       */
+/*   Updated: 2025/10/12 22:55:19 by ssaensuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	ft_check_format(char *format, va_list argp)
+static int	ft_check_format(const char *format, va_list argp)
 {
-	int	len;
-
-	len = 0;
-	// char	test; // test variable
-	
-	// test
-	// test = (char)va_arg(argp, int);
-	
-	// printf("argp_check: %c\n", test);
 	if (*format == 'c')
-		len += ft_putchar((char)va_arg(argp, int));
-	// else if (c == 's')
-	// 	len += ft_putstr(va_arg(argp, char *));
-	// else if (c == 'p')
-	// 	len += ft_putptr(va_arg(argp, void *));
-	// else if (c == 'd')
-	// 	len += ft_putnbr(va_arg(argp, int));
-	// else if (c == 'i')
-	// 	len += ft_putnbr(va_arg(argp, int));
-	// else if (c == 'u')
-	// 	len += ft_putunbr(va_arg(argp, unsigned int));
-	// else if (c == 'x')
-	// 	len += ft_puthex(va_arg(argp, int));
-	// else if (c == 'X')
-	// 	len += ft_puthex(va_arg(argp, int));
+		return (ft_putchar((char)va_arg(argp, int)));
+	else if (*format == 's')
+		return (ft_putstr(va_arg(argp, char *)));
+	else if (*format == 'p')
+		return (ft_putptr((unsigned long)va_arg(argp, void *)));
+	else if (*format == 'd' || *format == 'i')
+		return (ft_putnbr(va_arg(argp, int)));
+	else if (*format == 'u')
+		return (ft_putunbr(va_arg(argp, unsigned int)));
+	else if (*format == 'x' || *format == 'X')
+		return (ft_puthex(va_arg(argp, unsigned int), *format));
 	else if (*format == '%')
-		len += ft_putchar('%');
-	
-	return (len);
+		return (ft_putchar('%'));
+	return (0);
 }
 
-int ft_printf(char* format, ...)
+int	ft_printf(const char *format, ...)
 {
-    va_list argp;
+	va_list	argp;
 	int		len;
-	// char	test; // test variable
-	
+
 	va_start(argp, format);
-	// test
-	// test = (char)va_arg(argp, int);
-	
-	// printf("argp: %c\n", test);
 	len = 0;
 	while (*format)
 	{
-		if (*format == '%')
+		if (*format == '%' && !(*(format + 1)))
+			break ;
+		else if (*format == '%')
 		{
 			format++;
-			// printf("format %c\n", *format);
 			len += ft_check_format(format, argp);
 		}
 		else
 			len += ft_putchar(*format);
 		format++;
-		// printf("format: %c\n", *format);
 	}
 	va_end(argp);
 	return (len);
-}
-
-int main()
-{
-	int len;
-
-	len = ft_printf("Hello %c, your score is %%", 'T');
-	printf("Length: %d\n", len);
-	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ssaensuk <ssaensuk@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 19:21:05 by ssaensuk          #+#    #+#             */
-/*   Updated: 2025/10/11 20:13:40 by ssaensuk         ###   ########.fr       */
+/*   Updated: 2025/10/12 22:46:02 by ssaensuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,12 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int ft_printf(char* format, ...);
-int ft_putchar(char c);
+int	ft_printf(const char *format, ...);
+int	ft_putchar(char c);
+int	ft_putstr(char *str);
+int	ft_putnbr(int n);
+int	ft_putunbr(unsigned int n);
+int	ft_putptr(unsigned long ptr);
+int	ft_puthex(unsigned int n, char format);
 
 #endif
